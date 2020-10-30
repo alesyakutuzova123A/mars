b93d80f09cf94bb4df37b76488df9618ae0376c4
windows <kutuzova@yandex.ru>
2020-10-30 15:54:03 +0300

    1

---
+++ script.js
@@ -0,0 +1,26 @@
+
+  $(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots:true,
+        slidesToShow:1,
+        autoplay: false,
+        speed:1000,
+        autoplaySpeed:1000,
+        responsive:[
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow:1
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow:1
+                }
+            }
+        ]
+    });
+});
+
